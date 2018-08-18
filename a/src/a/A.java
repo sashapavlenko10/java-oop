3a5c7b138a3b2b41cc6362ae5e6ebc36eef18726
Sasha <Sasha@DESKTOP-2HNPLU4>
2018-08-18 12:44:47 +0300

    First commit

---
+++ a/src/a/A.java
@@ -0,0 +1,10 @@
+package a;
+public class A{
+    public static void main(String[] args) {
+        Runtime r = Runtime.getRuntime();
+        System.out.println(System.getProperty("os.name"));
+        System.out.println(System.getProperty("os.version"));
+        System.out.println(r. availableProcessors());
+        System.out.println(r. totalMemory());
+    }
+}
