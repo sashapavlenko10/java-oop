e745fe13a3bd61091daac5167dc07429d7e45870
sashapavlenko10 <pavlenko1004@GMAIL.COM>
2018-08-24 15:31:44 +0300

    Update Loop4.java

--- src/Loop4.java
+++ src/Loop4.java
@@ -2,21 +2,10 @@
 public class Loop4 {
 
 	public static void main(String[] args) {
-		/*for(int i =10; i<=20;i++) {
-		System.out.println(i);
-		int i=9;
+		int i = 9;
 		while(i<20) {
 			++i ;
-		System.out.println(i);
-	int [] x = {10,11,12,13,14,15,16,17,18,19,20};
-	for(int i : x) {
-		System.out.println(i);*/
-	int i=9;
-	do {
-		++i ;
-	System.out.println(i);
-	   } while(i<20);
-
+		System.out.println(i);}
 	}
 
 }
