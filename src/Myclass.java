d3c93c304a9c4547899f76b75cf86db4a133d316
Sasha <Sasha@DESKTOP-2HNPLU4>
2018-08-24 12:23:57 +0300

    First class

---
+++ src/Myclass.java
@@ -0,0 +1,9 @@
+ public class Myclass{
+public static void main(String[] args) {
+        Runtime r = Runtime.getRuntime();
+        System.out.println(System.getProperty("os.name"));
+        System.out.println(System.getProperty("os.version"));
+        System.out.println(r. availableProcessors());
+        System.out.println(r. totalMemory());
+    }
+}
