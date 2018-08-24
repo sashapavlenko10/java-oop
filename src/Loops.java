fbd321543f5d2b19a022dec7bdde81dd0522eb99
Sasha <Sasha@DESKTOP-2HNPLU4>
2018-08-24 15:11:06 +0300

    Loops

---
+++ src/Loops.java
@@ -0,0 +1,21 @@
+
+public class Loops {
+
+	public static void main(String[] args) {
+		/*for(int i =10; i<=20;i++) {
+			System.out.println(i);
+			int i=9;
+			while(i<20) {
+				++i ;
+			System.out.println(i);
+		int [] x = {10,11,12,13,14,15,16,17,18,19,20};
+		for(int i : x) {
+			System.out.println(i);*/
+		int i=9;
+		do {
+			++i ;
+		System.out.println(i);
+    	   } while(i<20);
+		
+	  }
+}
