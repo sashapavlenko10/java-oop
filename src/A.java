bea615c853e55dbdcc4334a7ab0ae436b37a4225
Sasha <Sasha@DESKTOP-2HNPLU4>
2018-08-25 23:30:28 +0300

    array bytes

---
+++ src/A.java
@@ -0,0 +1,17 @@
+
+public class A {
+	public static void main(String[] args) {	
+		int [] a = {1,2,3,4,5};
+              met(a);
+
+	}
+
+	private static void met(int[]a){
+		int sum;
+		sum =a.length*4;
+		System.out.println("array have: \0"+sum+"\0 bytes");
+	}
+
+}
+
+
