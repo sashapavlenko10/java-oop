4d5b7687ff9131282466b6d70d446498a24b0f66
Sasha <Sasha@DESKTOP-2HNPLU4>
2018-08-26 15:19:56 +0300

    1

--- src/Myclass.java
+++ src/Myclass.java
@@ -5,5 +5,6 @@ public static void main(String[] args) {
         System.out.println(System.getProperty("os.version"));
         System.out.println(r. availableProcessors());
         System.out.println(r. totalMemory());   
+          
     }
 }
