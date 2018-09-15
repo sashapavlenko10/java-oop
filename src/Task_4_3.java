fa49a6ea0f92c0d1d2b6ea84c0c460f46e686c52
Sasha <Sasha@DESKTOP-2HNPLU4>
2018-09-15 13:42:29 +0300

    task 4 3

---
+++ src/Task_4_3.java
@@ -0,0 +1,11 @@
+package src;
+
+public class Task_4_3 {
+public static void main(String[] args) {
+	int array []  = new int[101];
+	for(int i = 0; i<=100; i++) {
+		array[i]=100-i;
+		System.out.println(array[i]);
+	}
+}
+}
