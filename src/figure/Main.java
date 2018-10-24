fd438a2f3602f7c20e5eae4fab6cc7bc96a33958
Sasha <Sasha@DESKTOP-2HNPLU4>
2018-10-24 20:17:46 +0300

    figure

---
+++ src/figure/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+public static void main(String [] argv) {
+	int a = 5; int b = 7;
+Circle circle - new Circle();
+System.out.println(circle);
+Rectangle rectangle = new Rectangle();
+System.out.println(rectangle);
+Triangle triangle = new Triangle();
+System.out.println(triangle);
+}
